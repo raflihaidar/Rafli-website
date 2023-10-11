af6d5253320351c48cb583b92324fa55c54c576c
rafli.haidar30@gmail.com <rafli.haidar30@gmail.com>
2023-10-11 09:47:11 +0700

    fixing bug

--- src/components/SkillsCardComponent.vue
+++ src/components/SkillsCardComponent.vue
@@ -1,62 +1,62 @@
 <template>
   <main class="w-full h-full grid lg:grid-cols-4 grid-cols-1 gap-8">
     <a href="https://www.javascript.com/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/js.png' alt="js" class="w-8">
       <p>Javascript</p>
     </a>
     <a href="https://react.dev/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/react.png' alt="react" class="w-8">
       <p>React</p>
     </a>
     <a href="https://vuejs.org/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/vue.svg' alt="vue" class="w-8">
       <p>Vue</p>
     </a>
     <a href="https://expressjs.com/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/express.png' alt="express" class="w-8">
       <p>Express</p>
     </a>
     <a href="https://www.w3schools.com/cpp/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/c++.png' alt="c++" class="w-8">
       <p>c++</p>
     </a>
     <a href="https://www.java.com/en/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/java.png' alt="java" class="w-8">
       <p>Java</p>
     </a>
     <a href="https://tailwindcss.com/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/tailwind.svg' alt="tailwind" class="w-8">
       <p>Tailwind css</p>
     </a>
     <a href="https://getbootstrap.com/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/bootstrap.png' alt="Bootstrap" class="w-8">
       <p>Bootstrap</p>
     </a>
     <a href="https://www.mysql.com/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/mysql.png' alt="Mysql" class="w-8">
       <p>Mysql</p>
     </a>
     <a href="https://pinia.vuejs.org/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/pinia.svg' alt="Pinia" class="w-8">
       <p>Pinia</p>
     </a>
     <a href="https://redux.js.org/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/redux.png' alt="redux" class="w-8">
       <p>Redux</p>
     </a>
     <a href="https://github.com/" target="_blank"
-      class="bg-sky-950 flex gap-x-3 items-center hover:bg-blue-900 hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
+      class="bg-sky-950 flex gap-x-3 items-center lg:hover:bg-blue-900 lg:hover:bg-opacity-50 transition-colors  px-5 py-5 text-center text-slate-200 text-lg font-bold cursor-pointer">
       <img src='../assets/img/github.png' alt="github" class="w-8">
       <p>Github</p>
     </a>
