043a78b3d4dee4d6de9b351eb1ca1c2db91cceae
Rafli Haidar <rafli.haidar30@gmail.com>
2024-04-13 22:10:39 +0700

    feat : add new achivement, experience, project section

--- src/views/EducationView.vue
+++ src/views/EducationView.vue
@@ -3,14 +3,19 @@
     <div class="w-[80%] mx-auto my-10">
       <section class="mb-20">
         <SubTittleComponent name="EDUCATION" />
-        <div class="lg:w-[35%] w-full bg-sky-950 hover:bg-blue-950 transition-colors ease-in-out rounded-b-xl">
+        <div
+          class="lg:w-[35%] w-full bg-sky-950 hover:bg-blue-950 transition-colors ease-in-out rounded-b-xl"
+        >
           <span>
-            <img src="../assets//img/Telkom-University.jpg" alt="Telkom University">
+            <img src="../assets//img/Telkom-University.jpg" alt="Telkom University" />
           </span>
           <div class="w-full text-white px-2 py-3">
-            <a href="https://ittelkom-sby.ac.id/" target="_blank"
-              class="text-xl font-semibold hover:underline cursor-pointer transition">Telkom University Campus
-              Surabaya</a>
+            <a
+              href="https://ittelkom-sby.ac.id/"
+              target="_blank"
+              class="text-xl font-semibold hover:underline cursor-pointer transition"
+              >Telkom University Campus Surabaya</a
+            >
             <p class="text-lg">Sep 2022 - Present</p>
             <p class="text-md italic">Bachelor;s Degree in Information Technology - S.Kom.</p>
           </div>
@@ -19,15 +24,22 @@
 
       <section class="py-10">
         <SubTittleComponent name="CERTIFICATIONS" />
-        <div class="grid lg:grid-cols-3 pb-5 grid-cols-1 gap-8 text-slate-200 font-semibold text-xl text-center">
+        <div
+          class="grid lg:grid-cols-3 pb-5 grid-cols-1 gap-8 text-slate-200 font-semibold text-xl text-center"
+        >
           <span v-for="(item, index) in certificate" :key="index">
-            <div class="relative border-4 mt-3 border-slate-200 cursor-pointer" @mouseover="handleHover(index, true)"
-              @mouseleave="handleHover(index, false)">
+            <div
+              class="relative border-4 mt-3 border-slate-200 cursor-pointer"
+              @mouseover="handleHover(index, true)"
+              @mouseleave="handleHover(index, false)"
+            >
               <div>
-                <img :src="item.img" alt="mitra pasar" class="w-full">
+                <img :src="item.img" alt="mitra pasar" class="w-full" />
               </div>
-              <span v-if="hoverIndex === index"
-                class="absolute animate-fadeout flex gap-x-3 items-center justify-center bg-black opacity-80 w-full h-full top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-10">
+              <span
+                v-if="hoverIndex === index"
+                class="absolute animate-fadeout flex gap-x-3 items-center justify-center bg-black opacity-80 w-full h-full top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-10"
+              >
                 <ZoomIcon @click="openModal(item)" size="1.5em" />
                 <a :href="item.url" target="_blank">
                   <LinkIcon />
@@ -38,7 +50,48 @@
           </span>
         </div>
         <transition to="projectView">
-          <ModalComponent name="certificate" :status="modalStatus" :data="modalContent" @close-modal="closeModal" />
+          <ModalComponent
+            name="certificate"
+            :status="modalStatus"
+            :data="modalContent"
+            @close-modal="closeModal"
+          />
+        </transition>
+      </section>
+      <section class="py-10">
+        <SubTittleComponent name="ACHIVEMENT" />
+        <div
+          class="grid lg:grid-cols-3 pb-5 grid-cols-1 gap-8 text-slate-200 font-semibold text-xl text-center"
+        >
+          <span v-for="(item, index) in achivement" :key="index">
+            <div
+              class="relative border-4 mt-3 border-slate-200 cursor-pointer"
+              @mouseover="handleHover(index, true)"
+              @mouseleave="handleHover(index, false)"
+            >
+              <div>
+                <img :src="item.img" :alt="item.name" class="w-full" />
+              </div>
+              <span
+                v-if="hoverIndex === index"
+                class="absolute animate-fadeout flex gap-x-3 items-center justify-center bg-black opacity-80 w-full h-full top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-10"
+              >
+                <ZoomIcon @click="openModal(item)" size="1.5em" />
+                <a :href="item.url" target="_blank">
+                  <LinkIcon />
+                </a>
+                <CopyIcon @click="copyToClipBoard(item.url)" />
+              </span>
+            </div>
+          </span>
+        </div>
+        <transition to="projectView">
+          <ModalComponent
+            name="certificate"
+            :status="modalStatus"
+            :data="modalContent"
+            @close-modal="closeModal"
+          />
         </transition>
       </section>
     </div>
@@ -46,17 +99,18 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import SubTittleComponent from '../components/SubTittleComponent.vue';
-import InosoftCertificate from "../assets//img/sertifikat-inosoft.jpg"
-import JavascriptCertif from "../assets//img/js-certification.jpg"
-import ModalComponent from "../components/ModalComponent.vue"
+import { ref } from 'vue'
+import SubTittleComponent from '../components/SubTittleComponent.vue'
+import InosoftCertificate from '../assets//img/sertifikat-inosoft.jpg'
+import JavascriptCertif from '../assets//img/js-certification.jpg'
+import InnovillageCertification from '../assets/img/sertifikat-Innovillage.jpg'
+import ModalComponent from '../components/ModalComponent.vue'
 import ZoomIcon from '../components/icons/ZoomIcon.vue'
 import LinkIcon from '../components/icons/LinkIcon.vue'
 import CopyIcon from '../components/icons/CopyIcon.vue'
 
-let hoverIndex = ref(null);
-let modalStatus = ref(false);
+let hoverIndex = ref(null)
+let modalStatus = ref(false)
 let modalContent = ref(null)
 
 const handleHover = (index, isHover) => {
@@ -76,10 +130,10 @@ const closeModal = () => {
 const copyToClipBoard = async (url) => {
   try {
     // Copy the text inside the text field
-    await navigator.clipboard.writeText(url);
+    await navigator.clipboard.writeText(url)
 
     // Alert the copied text
-    alert("Link copied!");
+    alert('Link copied!')
   } catch (error) {
     console.log(error)
   }
@@ -87,15 +141,24 @@ const copyToClipBoard = async (url) => {
 const certificate = [
   {
     id: 1,
-    name: "Inosoft Bootcamp",
+    name: 'Inosoft Bootcamp',
     img: InosoftCertificate,
-    url: ""
+    url: ''
   },
   {
     id: 2,
-    name: "Javascript FreeCodeCamp",
+    name: 'Javascript FreeCodeCamp',
     img: JavascriptCertif,
-    url: "https://www.freecodecamp.org/certification/fccf9986e11-1a49-43b3-98b9-c16b4dec8773/javascript-algorithms-and-data-structures"
+    url: 'https://www.freecodecamp.org/certification/fccf9986e11-1a49-43b3-98b9-c16b4dec8773/javascript-algorithms-and-data-structures'
+  }
+]
+
+const achivement = [
+  {
+    id: 1,
+    name: 'Innovillage 2023',
+    img: InnovillageCertification,
+    url: ''
   }
 ]
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,74 +1,94 @@
 <template>
-  <main class="absolute w-[80%] top-[50%] left-[50%] lg:text-left text-center -translate-x-[50%] -translate-y-[50%]">
-    <section class="lg:w-[35%]">
-      <p class="lg:text-5xl text-3xl font-bold text-slate-200 hover:underline transition cursor-pointer">Rafli Haidar
-        Nashif</p>
+  <main
+    class="absolute w-[80%] top-[50%] left-[50%] lg:text-left text-center -translate-x-[50%] -translate-y-[50%]"
+  >
+    <section class="lg:w-[50%]">
+      <p
+        class="lg:text-5xl text-3xl font-bold text-slate-200 hover:underline transition cursor-pointer"
+      >
+        Rafli Haidar Nashif
+      </p>
     </section>
     <section class="my-10">
       <p class="text-2xl text-slate-200 font-bold">Hi, Folks!</p>
       <span class="lg:text-2xl text-xl text-green-700 font-bold">{{ description }}</span>
-      <span class="w-[3px] ml-[4px] inline-block bg-white" :class="!typedStatus ? 'animate-blink' : ''">&nbsp;</span>
+      <span
+        class="w-[3px] ml-[4px] inline-block bg-white"
+        :class="!typedStatus ? 'animate-blink' : ''"
+        >&nbsp;</span
+      >
     </section>
     <section class="lg:w-[15%] gap-x-5 flex lg:justify-between justify-center my-10">
-      <span class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3">
+      <span
+        class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3"
+      >
         <InstagramIcon />
       </span>
-      <span class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3">
+      <span
+        class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3"
+      >
         <GithubIcon url="https://github.com/raflihaidar" size="1em" />
       </span>
-      <span class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3">
+      <span
+        class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3"
+      >
         <LinkedinIcon />
       </span>
-      <span class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3">
+      <span
+        class="w-auto bg-gray-800 hover:bg-green-700 transition-colors cursor-pointer rounded-full p-3"
+      >
         <EmailIcon size="1" />
       </span>
     </section>
     <section>
       <button
-        class="bg-blue-600 hover:bg-blue-700 transition-colors py-2 px-4 text-white font-semibold rounded-lg text-sm lg:w-40 w-52">
+        class="bg-blue-600 hover:bg-blue-700 transition-colors py-2 px-4 text-white font-semibold rounded-lg text-sm lg:w-40 w-52"
+      >
         View My Resume
       </button>
     </section>
   </main>
-  <footer class="absolute bottom-2 bg-inherit left-2/4  text-center -translate-x-[50%] -translate-y-[50%]">
+  <footer
+    class="absolute bottom-2 bg-inherit left-2/4 text-center -translate-x-[50%] -translate-y-[50%]"
+  >
     <FooterComponent />
   </footer>
 </template>
 
 <script setup>
-import InstagramIcon from "../components/icons/InstagramIcon.vue"
-import GithubIcon from "../components/icons/GithubIcon.vue"
-import LinkedinIcon from "../components/icons/LinkedinIcon.vue"
-import EmailIcon from "../components/icons/EmailIcon.vue"
-import FooterComponent from "../components/FooterComponent.vue"
-import { ref } from 'vue';
+import InstagramIcon from '../components/icons/InstagramIcon.vue'
+import GithubIcon from '../components/icons/GithubIcon.vue'
+import LinkedinIcon from '../components/icons/LinkedinIcon.vue'
+import EmailIcon from '../components/icons/EmailIcon.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+import { ref } from 'vue'
 
-let stringIndex = 0;
-let typedStatus = ref(true);
-const text = ["I am a Web Developer", "Especially a Front end Developer"];
-let arrIndex = 0;
-let description = ref("");
+let stringIndex = 0
+let typedStatus = ref(true)
+const text = ['I am a Web Developer', 'Especially a Front end Developer']
+let arrIndex = 0
+let description = ref('')
 
 const typedText = () => {
   if (stringIndex < text[arrIndex].length) {
-    description.value += text[arrIndex].charAt(stringIndex);
-    stringIndex += 1;
-    setTimeout(typedText, 100);
+    description.value += text[arrIndex].charAt(stringIndex)
+    stringIndex += 1
+    setTimeout(typedText, 100)
   } else {
-    typedStatus.value = false;
-    setTimeout(eraseText, 2000);
+    typedStatus.value = false
+    setTimeout(eraseText, 2000)
   }
 }
 
 const eraseText = () => {
   typedStatus.value = true
-  if (typedStatus.value && description.value != "") {
-    description.value = description.value.substring(0, stringIndex - 1);
-    stringIndex--;
-    setTimeout(eraseText, 100);
+  if (typedStatus.value && description.value != '') {
+    description.value = description.value.substring(0, stringIndex - 1)
+    stringIndex--
+    setTimeout(eraseText, 100)
   } else {
-    arrIndex = (arrIndex + 1) % text.length;
-    setTimeout(typedText, 100);
+    arrIndex = (arrIndex + 1) % text.length
+    setTimeout(typedText, 100)
   }
 }
 
