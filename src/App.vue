334afc0571c9bb77c01dc7ea5cd74cae4359d238
raflihaidar <rafli.haidar30@gmail.com>
2024-08-18 07:47:54 +0700

    feat () : change layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="bg-slate-95 font-display selection:bg-green-700">
-    <NavbarComponent />
-    <router-view></router-view>
+  <div class="bg-slate-950 w-screen h-screen font-display selection:bg-green-700 scroll-smooth">
+    <BaseNavbar />
+    <header>
+      <HomeView />
+    </header>
+    <main>
+      <AboutView />
+      <EducationView />
+      <ExperienceView />
+      <ProjectView />
+    </main>
+    <BaseFooter />
   </div>
 </template>
 
 <script setup>
-import NavbarComponent from './components/NavbarComponent.vue'
+import BaseNavbar from './components/BaseNavbar.vue'
+import BaseFooter from './components/BaseFooter.vue'
+import HomeView from './views/HomeView.vue'
+import AboutView from './views/AboutView.vue'
+import EducationView from './views/EducationView.vue'
+import ExperienceView from './views/ExperienceView.vue'
+import ProjectView from './views/ProjectView.vue'
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import './assets/main.css'
 import { MotionPlugin } from '@vueuse/motion'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
-app.use(router)
 app.use(MotionPlugin)
 app.mount('#app')
