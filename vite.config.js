19a8cfd989b2bc9b2fe2f89f1160e68d828b8505
Rafli Haidar Nashif <rafli.haidar30@gmail.com>
2025-04-06 18:14:38 +0700

    upgrade tailwind 3.4

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     viteCompression({
       algorithm: 'gzip'
     }),
